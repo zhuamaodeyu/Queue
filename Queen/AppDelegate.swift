f593ec7af681b8733c66515d9cb77e28044f5d65
zhuamaodeyu <playtomandjerry@gmail.com>
2019-07-05 23:22:20 +0800

    Initial Commit

---
+++ Queen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Queen
+//
+//  Created by 聂子 on 2019/7/5.
+//  Copyright © 2019 聂子. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Queen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Queen
+//
+//  Created by 聂子 on 2019/7/5.
+//  Copyright © 2019 聂子. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
