eabae0d78f7269cb284b47e4d187fd88eadc1df2
抓猫的鱼 <playtomandjerry@gmail.com>
2019-11-23 16:29:16 +0800

    feat: add menu appdelegate extensions

---
+++ Queen/AppDelegate/AppDelegate+Menu.swift
@@ -0,0 +1,53 @@
+//
+//  AppDelegate+Menu.swift
+//  Queen
+//
+//  Created by 聂子 on 2019/11/23.
+//  Copyright © 2019 聂子. All rights reserved.
+//
+
+import Cocoa
+
+
+// MAKR: menu
+// MARK: - menu action
+extension AppDelegate {
+    /// activate self and perform "New" menu action
+    @IBAction func newDocumentActivatingApplication(_ sender: Any?) {
+        
+        NSApp.activate(ignoringOtherApps: true)
+        NSDocumentController.shared.newDocument(sender)
+    }
+    
+    
+    /// activate self and perform "Open..." menu action
+    @IBAction func openDocumentActivatingApplication(_ sender: Any?) {
+        
+        NSApp.activate(ignoringOtherApps: true)
+        NSDocumentController.shared.openDocument(sender)
+    }
+
+}
+
+// MARK: Menu Application
+extension AppDelegate {
+    // 关于
+    @IBAction func aboutApplication(_ sender: Any?) {
+        
+    }
+    //
+    @IBAction func preferences(_ sender: Any?) {
+        
+    }
+}
+
+// MARK: Menu file
+extension AppDelegate {
+    
+}
+// MARK:
+extension AppDelegate {
+    
+}
+
+
--- Queen/AppDelegate/AppDelegate.swift
+++ Queen/AppDelegate/AppDelegate.swift
@@ -150,35 +150,3 @@ extension AppDelegate {
     }
 
 }
-
-
-// MARK: - menu action
-extension AppDelegate {
-    /// activate self and perform "New" menu action
-    @IBAction func newDocumentActivatingApplication(_ sender: Any?) {
-
-        NSApp.activate(ignoringOtherApps: true)
-        NSDocumentController.shared.newDocument(sender)
-    }
-
-
-    /// activate self and perform "Open..." menu action
-    @IBAction func openDocumentActivatingApplication(_ sender: Any?) {
-
-        NSApp.activate(ignoringOtherApps: true)
-        NSDocumentController.shared.openDocument(sender)
-    }
-    /// show preferences window
-    @IBAction func showPreferences(_ sender: Any?) {
-
-
-    }
-    /// open bug report page in default web browser
-    @IBAction func reportBug(_ sender: Any?) {
-
-
-    }
-}
-
-
-
