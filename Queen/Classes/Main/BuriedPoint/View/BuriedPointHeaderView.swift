24a83945dc1eb154d7750913aefe12308123ff9f
抓猫的鱼 <playtomandjerry@gmail.com>
2019-11-27 23:52:49 +0800

    feat: add header View

---
+++ Queen/Classes/Main/BuriedPoint/View/BuriedPointHeaderView.swift
@@ -0,0 +1,24 @@
+//
+//  BuriedPointHeaderView.swift
+//  Queen
+//
+//  Created by 聂子 on 2019/11/27.
+//  Copyright © 2019 聂子. All rights reserved.
+//
+
+import Cocoa
+
+
+class BuriedPointHeaderView: NSView {
+
+    
+}
+
+extension BuriedPointHeaderView {
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        
+        // Drawing code here.
+    }
+}
